9c832d34d5118e544c4668290505e9e83437feba
Tuấn Anh Lâm <93982269+TuanAnhHaui@users.noreply.github.com>
2022-05-12 00:00:10 +0700

    add GenerateJWT

--- main.go
+++ main.go
@@ -3,17 +3,21 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	"golang.org/x/crypto/bcrypt"
 	"log"
 	"net/http"
+	"time"
 )
-func SetError(err Error, message string) Error {
-	err.IsError = true
-	err.Message = message
-	return err
-}
-var router *mux.Router
+
+var (
+	router *mux.Router
+	secretkey string ="secretkeyjwt"
+)
+
+
 
 func CreateRouter() {
 	router = mux.NewRouter()
@@ -27,12 +31,12 @@ type User struct {
 	gorm.Model
 	Name     string `json:"name"`
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password string `json:"-"`
 	Role     string `json:"role"`
 }
 type Authentication struct {
 	Email    string `json:"email"`
-	Password string `json:"password"`
+	Password string `json:"-"`
 }
 type Token struct {
 	Role        string `json:"role"`
@@ -44,7 +48,12 @@ type Error struct {
 	Message string `json:"message"`
 }
 
-
+//set error message in Error struct
+func SetError(err Error, message string) Error {
+	err.IsError = true
+	err.Message = message
+	return err
+}
 func GetDatabase() *gorm.DB {
 	databasename := "userdb"
 	database := "postgres"
@@ -52,7 +61,7 @@ func GetDatabase() *gorm.DB {
 	databaseurl := "postgres://postgres:" + databasepassword + "@localhost/" + databasename + "?sslmode=disable"
 	connection, err := gorm.Open(database, databaseurl)
 	if err != nil {
-		log.Fatal("wrong database url ")
+		log.Fatal("Invalid database url ")
 	}
 	sqldb := connection.DB
 
@@ -60,7 +69,7 @@ func GetDatabase() *gorm.DB {
 	if err != nil {
 		log.Fatal("database connected")
 	}
-	fmt.Println("connected to database")
+	fmt.Println("Database connection successful")
 	return connection
 }
 func InitialMigration() {
@@ -72,6 +81,52 @@ func Closedatabase(connection *gorm.DB) {
 	sqldb := connection.DB()
 	sqldb.Close()
 }
+func GeneratehashPassword(password string) (string,error) {
+	bytes,err :=bcrypt.GenerateFromPassword([]byte(password),14)
+	return string(bytes),err
+}
+func CheckPasswordHash(password ,hash string) bool {
+	err :=bcrypt.CompareHashAndPassword([]byte(hash),[]byte(password))
+	return err == nil
+}
+//GenerateJWT
+func GenerateJWT(email,role string)(string,error){
+	var mySigningKey =[]byte(secretkey)
+	token := jwt.New(jwt.SigningMethodHS256)
+	claims := token.Claims.(jwt.MapClaims)
+	claims["authorized"]=true
+	claims["email"]=email
+	claims["role"]=role
+	claims["expires"]=time.Now().Add(time.Minute *30).Unix()
+
+	tokenString,err := token.SignedString(mySigningKey)
+	if err != nil {
+		fmt.Errorf("Something went wrong :%s",err.Error())
+		return "",err
+	}
+	return tokenString,nil
+}
+
+//Middleware function
+//check wether user is authorized or not
+func IsAuthorized(handler http.HandlerFunc) http.HandlerFunc{
+	return func(w http.ResponseWriter,r *http.Request){
+		if r.Header["Token"] == nil{
+			var err Error
+			err =SetError(err,"No Token found")
+			json.NewDecoder(w).Encode(err)
+			return
+		}
+		var mySigningKey = []byte(secretkey)
+
+		token,err := jwt.Parse(r.Header["Token"][0],func(token *jwt.Token)(interface{},error))
+	}
+}
+
+
+
+
+
 func SignUp(w http.ResponseWriter, r *http.Request) {
 	connection := GetDatabase()
 	defer Closedatabase(connection)
@@ -79,7 +134,7 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 	var user User
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		var err error
+		var err Error
 		err = SetError (err, "Error reading body")
 		w.Header().Set("Content-Type", "application/json")
 		json.NewDecoder(w).Encode(err)
@@ -90,7 +145,7 @@ func SignUp(w http.ResponseWriter, r *http.Request) {
 var dbuser User
 connection.Where("email=?", user.Email).First((&dbuser))
 
-//cehck if email is already register or not
+//check if email is already register or not
 if dbuser.Email != ""{
 	var err error
 	err = SetError(err, "Email already registered")
